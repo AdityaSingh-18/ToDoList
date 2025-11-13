64c0901794e2fd35f00eb7e119a8099a1ce152dd
AdityaSingh <anshul8303858511@gmail.com>
2025-11-13 17:51:12 +0530

    Added Edit and Undone Tasks functionality

--- src/toDoList/DisplayList.jsx
+++ src/toDoList/DisplayList.jsx
@@ -1,26 +1,84 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 import './DisplayList.css';
 
 export default function DisplayList({v, index, toDoList, setToDoList}) {
-
+    let [editIndex, setEditIndex] = useState(null);
+    let [tempValue, setTempValue] = useState("");
     let [doneIndex, setDoneIndex] = useState([]);
 
-    let doneTask = (index) => {
-        setDoneIndex([...doneIndex, index]);   
-    }
+    let inputRef = useRef(null);
 
     let toDelete = (index) => {
-        let updatedList = toDoList.filter((v, i) => i !== index);
+        let updatedList = toDoList.filter((_, i) => i !== index);
         setToDoList(updatedList);
-    }
+    };
+
+    let editTask = (index) => {
+        setEditIndex(index);
+        setTempValue(toDoList[index]);
+        setTimeout(() => inputRef.current?.focus(), 0.3);
+    };
+
+    let saveEdit = (index) => {
+        let updatedTask = [...toDoList];
+        updatedTask[index] = tempValue;
+        setToDoList(updatedTask);
+        setEditIndex(null);
+        setTempValue("");
+    };
+
+    let cancelEdit = () => {
+        setEditIndex(null);
+        setTempValue("");
+    };
+
+    let handleChange = (event) => {
+        setTempValue(event.target.value);
+    };
+
+    let doneTask = (index) => {
+        setDoneIndex([...doneIndex, index]);
+    };
+
+    let undoneTask = (index) => {
+        let updatedDoneIndex = doneIndex.filter((v) => v !== index);
+        setDoneIndex(updatedDoneIndex);
+    };
 
     return (
         <div className={doneIndex.includes(index) ? "doneTasks toDoTasks" : "toDoTasks"}>
-            {v}
-            <span className="Icons">
-                  <i onClick={() => doneTask(index)} className="fas fa-check-circle"></i>
-                  <i onClick={() => toDelete(index)} className="fas fa-trash"></i>
-            </span>
+            {editIndex === index ? (
+                <>
+                    <input type="text" value={tempValue} onChange={handleChange} ref={inputRef} spellCheck="false" 
+                        onKeyDown={(e) => e.key === "Enter" && saveEdit(index)}
+                    />
+                    <span>
+                        <i onClick={() => saveEdit(index)} className="fas fa-save"></i>
+                        <i onClick={cancelEdit} className="fas fa-close"></i>
+                    </span>
+                </>
+            ) : (
+                <>
+                    {doneIndex.includes(index) ? (
+                        <>
+                            {v}
+                            <span>
+                                <i onClick={() => undoneTask(index)} className="fas fa-undo"></i>
+                                <i onClick={() => toDelete(index)} className="fas fa-trash"></i>
+                            </span>
+                        </>
+                    ) : (
+                        <>
+                            {v}
+                            <span>
+                                <i onClick={() => doneTask(index)} className="fas fa-check-circle"></i>
+                                <i onClick={() => editTask(index)} className="fas fa-edit"></i>
+                                <i onClick={() => toDelete(index)} className="fas fa-trash"></i>
+                            </span>
+                        </>
+                    )}
+                </>
+            )}
         </div>
-    )
+    );
 }
